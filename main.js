3f82d571abf0d1b87cd1a9940584f7ea06921be3
eswar2001 <teswar2001@yahoo.co.in>
2020-08-29 12:52:11 +0530

    content added

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var i = 0;
+    var txt = 'Registrations are open!'; /* The text */
+    var speed = 50; /* The speed/duration of the effect in milliseconds */
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("demo").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+    typeWriter();
+    $(".gallery-img").click(function () {
+        var t = $(this).attr("src");
+        $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
+        $("#myModal").modal();
+    });
+    $.getJSON('image.json', function (data) {
+        var pre = '<img src="';
+        var pos = '"alt="" width="100%" height="auto" class="gallery-img" />';
+        var str = '';
+        for (var image in data["image"]) {
+            str += pre + data["image"][image] + pos;
+        }
+        $('#gallery').append(str);
+        console.log(str);
+    });
+
+});
