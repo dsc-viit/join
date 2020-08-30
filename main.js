ac0ff4ca71f65cfcfc83e91a1da13436fac4b121
eswar2001 <teswar2001@yahoo.co.in>
2020-08-30 17:20:10 +0530

    design complete left for populating :smile:

--- main.js
+++ main.js
@@ -10,11 +10,7 @@ $(document).ready(function () {
         }
     }
     typeWriter();
-    $(".gallery-img").click(function () {
-        var t = $(this).attr("src");
-        $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
-        $("#myModal").modal();
-    });
+
     $.getJSON('https://raw.githubusercontent.com/dsc-viit/JSON-HUB/master/image.json', function (data) {
         var pre = '<img src="';
         var pos = '"alt="" width="100%" height="auto" class="gallery-img" />';
@@ -23,7 +19,16 @@ $(document).ready(function () {
             str += pre + data["image"][image] + pos;
         }
         $('#gallery').append(str);
-        console.log(str);
-    });
 
+        $('<img>').ready(function () {
+            console.log('Here you go guys');
+        });
+        console.log(str);
+    }).then(function () {
+        $(".gallery-img").click(function () {
+            var t = $(this).attr("src");
+            $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
+            $("#myModal").modal();
+        });
+    });
 });
